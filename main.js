9a0f8bb7f47a8bbaae614b631a3528130b5694a3
sunday stanley Idung <sundaystanley56@gmail.com>
2022-04-14 21:28:33 +0100

    Add html and styles to my project

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-const menu = document.querySelector('.menu')
-let menuOpen = false
-const activeMenu = document.querySelector('.list-container')
-const items = document.querySelectorAll(".list-item")
+const menu = document.querySelector('.menu');
+let menuOpen = false;
+const activeMenu = document.querySelector('.list-container');
+const items = document.querySelectorAll('.list-item');
 
 function toggleMenu() {
   if (!menuOpen) {
-    menu.classList.add('open')
-    menuOpen = true
-    activeMenu.style.left = '0'
+    menu.classList.add('open');
+    menuOpen = true;
+    activeMenu.style.left = '0';
   } else {
-    menu.classList.remove('open')
-    menuOpen = false
-    activeMenu.style.left = '-100%'
-
+    menu.classList.remove('open');
+    menuOpen = false;
+    activeMenu.style.left = '-100%';
   }
- 
 }
-menu.addEventListener('click', toggleMenu)
-items.forEach(item=>{
-  item.addEventListener('click', toggleMenu)
-})
+menu.addEventListener('click', toggleMenu);
+items.forEach((item) => {
+  item.addEventListener('click', toggleMenu);
+});
+
 
